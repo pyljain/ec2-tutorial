417052ea2960cabdd57364a45640f2e435e213bf
Payal Jain <pyl.jain3@gmail.com>
2017-10-28 12:00:31 +0100

    First Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.end('Hello')
+  res.end('Hello from Ubuntu')
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('Server Ready'))
